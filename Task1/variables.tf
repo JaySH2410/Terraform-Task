b9ff13e5d856dcb21aae84d923ba81f82e656245
Jay Sheth <jayamitsheth2001@gmail.com>
2024-07-10 10:39:33 +0530

    TASK1

--- Task1/variables.tf
+++ Task1/variables.tf
@@ -4,10 +4,8 @@ variable "project_id" {
 
 variable "location" {
   description = "The region to deploy resources"
-  default     = "us-west1"
 }
 
 variable "docker_image" {
   description = "The Docker image to deploy"
-  default     = "us-west1-docker.pkg.dev/noted-flux-428815-m3/aotask/aotask-image:latest"
 }
